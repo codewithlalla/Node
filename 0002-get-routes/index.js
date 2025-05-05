060351a2f4c6b3af49ab35e65e9b8f6c36d52af3
codewithlalla <codewithlalla@gmail.com>
2025-05-05 23:29:17 +0530

    add get routes tips

--- 0002-get-routes/index.js
+++ 0002-get-routes/index.js
@@ -11,6 +11,16 @@ app.get('/getAllUsers',(req, res)=>{
     res.send("this is getAllUsers Route")
 })
 
+app.get('/ab?cd',(req, res)=>{
+    res.send
+    ("run on abd && abcd")
+})
+
+app.get('/ab+cd',(req, res)=>{
+    res.send
+    ("run on ab && abcd &&abbcd etc")
+})
+
 
 app.listen(PORT,()=>{
     console.log("this node js server!!!!")
